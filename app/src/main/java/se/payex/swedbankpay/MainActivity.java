b07a9c4d988041236804971c97237a8125871e64
marlinshenoda <105081672+marlinshenoda@users.noreply.github.com>
2024-10-05 21:40:26 +0200

    AppToolbar

--- app/src/main/java/se/payex/swedbankpay/MainActivity.java
+++ app/src/main/java/se/payex/swedbankpay/MainActivity.java
@@ -1,12 +1,9 @@
 package se.payex.swedbankpay;
 
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        // Set up the  toolbar
+        Toolbar Toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(Toolbar);
+
+
     }
 }
